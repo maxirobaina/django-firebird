c941e4a3f2d6102a696dd88690130c4c7081a63b
Maximiliano Robaina <maxirobaina@gmail.com>
2012-10-26 20:56:29 -0300

    Added isolated model_fields test (copy from original django test suite)

--- tests/test_main/test_main/settings.py
+++ tests/test_main/test_main/settings.py
@@ -135,6 +135,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'test_base',
+    'model_fields',
 )
 
 # A sample logging configuration. The only tangible logging
