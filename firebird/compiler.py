e8f28a21dbd0a24c1073ce7d8ddaf10a2ea9532d
maxirobaina <maxirobaina@gmail.com>
2014-02-11 09:27:35 -0300

    Fixed #19 - missing SQLDateTimeCompiler

--- firebird/compiler.py
+++ firebird/compiler.py
@@ -52,3 +52,6 @@ class SQLAggregateCompiler(compiler.SQLAggregateCompiler, SQLCompiler):
 class SQLDateCompiler(compiler.SQLDateCompiler, SQLCompiler):
     pass
 
+class SQLDateTimeCompiler(compiler.SQLDateTimeCompiler, SQLCompiler):
+    pass
+
