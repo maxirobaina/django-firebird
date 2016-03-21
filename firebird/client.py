75616084023221e40678794c306b43f27e52b2c8
Maximiliano Robaina <maxirobaina@gmail.com>
2016-03-21 15:01:05 -0300

    Fixed #48 -- dbshell fails on Linux

--- firebird/client.py
+++ firebird/client.py
@@ -10,14 +10,16 @@ class DatabaseClient(BaseDatabaseClient):
     def _get_args(self):
         args = [self.executable_name]
         settings_dict = self.connection.settings_dict
-        if settings_dict['USER']:
-            args += ["-u", settings_dict['USER']]
-        if settings_dict['PASSWORD']:
-            args += ["-p", settings_dict['PASSWORD']]
         if settings_dict['HOST']:
             args.append(settings_dict['HOST'] + ':' + settings_dict['NAME'])
         else:
             args.append(settings_dict['NAME'])
+        if settings_dict['USER']:
+            args += ["-u", settings_dict['USER']]
+        if settings_dict['PASSWORD']:
+            args += ["-p", settings_dict['PASSWORD']]
+        if 'ROLE' in settings_dict:
+            args += ["-r", settings_dict['ROLE']]
         return args
     args = property(_get_args)
 
@@ -25,5 +27,5 @@ class DatabaseClient(BaseDatabaseClient):
         if os.name == 'nt':
             sys.exit(os.system(" ".join(self.args)))
         else:
+            self.executable_name = 'isql-fb'
             os.execvp(self.executable_name, self.args)
-        #os.system(' '.join(args))
