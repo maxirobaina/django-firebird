78d28e8a085522f9be82cfc6b143be1d79870bc6
maxirobaina <maxirobaina@gmail.com>
2022-04-25 14:13:13 -0300

    Put setuptools config into a separate .cfg file

--- setup.py
+++ setup.py
@@ -1,43 +1,12 @@
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme:
-    __doc__ = readme.read()
-
 
 # Provided as an attribute, so you can append to these instead
 # of replicating them:
 standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak', '*.orig')
 standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
 
-
-# Dynamically calculate the version based on firebird.VERSION.
-version = __import__('firebird').get_version()
-
 setup(
-    name='django-firebird',
-    version=version,
-    description='Firebird backend for Django web framework',
-    long_description=__doc__,
-    license='BSD',
-    author='Maximiliano Robaina',
-    author_email='maxirobaina@gmail.com',
-    url='https://github.com/maxirobaina/django-firebird',
     packages=find_packages(),
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Database',
-        'Topic :: Internet :: WWW/HTTP',
-    ],
-    zip_safe=False,
     install_requires=[],
 )
